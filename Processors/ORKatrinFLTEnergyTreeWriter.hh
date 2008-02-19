c88a1b71d5c3de5e5994d7c096752a05e8c476bd
bergmann <bergmann@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-02-19 19:09:22 +0000

    Reimplemented EndRun() to omit empty trees.
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@567 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Processors/ORKatrinFLTEnergyTreeWriter.hh
+++ Processors/ORKatrinFLTEnergyTreeWriter.hh
@@ -12,8 +12,10 @@ class ORKatrinFLTEnergyTreeWriter : public ORVTreeWriter
     ORKatrinFLTEnergyTreeWriter(std::string treeName = "");
     virtual ~ORKatrinFLTEnergyTreeWriter();
     virtual EReturnCode ProcessMyDataRecord(UInt_t* record);
+    virtual EReturnCode EndRun();
     virtual inline void Clear() 
-      { fSec = 0; fSubSec = 0; fCrate = 0; fCard = 0; fChannel = 0; fEnergy = 0; fEventID = 0;}
+      { fSec = 0; fSubSec = 0; fCrate = 0; fCard = 0; fChannel = 0; fEnergy = 0; fEventID = 0;
+        saveOnlyNonemptyTrees=true; }
   protected:
     virtual EReturnCode InitializeBranches();
 
@@ -22,6 +24,7 @@ class ORKatrinFLTEnergyTreeWriter : public ORVTreeWriter
     UInt_t fSec, fSubSec, fEventID, fChannelMap, fPageNumber;
     UShort_t fCrate, fCard, fChannel;
     UInt_t fEnergy, fResetSec, fResetSubSec;
+    Bool_t saveOnlyNonemptyTrees; //!< flag to skip writing empty trees -tb- 2008-02-19
 };
 
 #endif
