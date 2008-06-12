e5884a039125928edc653337a230e13b2ecdd8f2
jasondet <jasondet@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-06-12 22:52:38 +0000

    Properly implement filling for multi-dimensional histograms. Allow for y- and z-axis titles. Give a default binning for histograms that assumes the axis values are integers running from 0 to nbins-1. Jason
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@988 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Decoders/ORVHistDecoder.hh
+++ Decoders/ORVHistDecoder.hh
@@ -12,31 +12,34 @@ class ORVHistDecoder : virtual public ORVDataDecoder
     ORVHistDecoder() {}
     virtual ~ORVHistDecoder() {}
 
-    // functions for histogram booking
+    // Functions for histogram booking. By default, the x-binning assumes
+    // the x-values are integers running from 0 to NbinsX-1.
     virtual std::string GetHistName(int iHist) = 0;
     virtual std::string GetHistTitle(int iHist) = 0;
     virtual std::string GetXTitle() = 0;
     virtual size_t GetNbinsX() = 0;
-    virtual double GetXLo() = 0;
-    virtual double GetXHi() = 0;
+    virtual double GetXLo() { return -0.5; }
+    virtual double GetXHi() { return GetNbinsX() - 0.5; }
 
-    // functions for histogram filling
+    // Functions for histogram filling
     virtual int GetHistIndex(UInt_t* record) = 0; // user must define an indexing scheme
     virtual size_t GetNEntries(UInt_t* /*record*/) { return 1; }
     virtual double GetX(UInt_t* record, size_t i) = 0; // the variable to be histogrammed
     virtual double GetWeight(UInt_t* /*record*/, size_t /*i*/) // optional: weight the entry
       { return 1.0; } 
 
-    // optional: multi-dimensional
+    // Optional: multi-dimensional
     virtual inline size_t GetNDim() { return 1; }
+    virtual std::string GetYTitle() { return "Y"; }
     virtual inline double GetY(UInt_t* /*record*/, size_t /*i*/) { return 0.0; }
+    virtual inline size_t GetNbinsY() { return 1; }
+    virtual inline double GetYLo() { return -0.5; }
+    virtual inline double GetYHi() { return GetNbinsY() - 0.5; }
+    virtual std::string GetZTitle() { return "Z"; }
     virtual inline double GetZ(UInt_t* /*record*/, size_t /*i*/) { return 0.0; }
-    virtual inline size_t GetNbinsY() { return 0; }
-    virtual inline double GetYLo() { return 0.0; }
-    virtual inline double GetYHi() { return 0.0; }
-    virtual inline size_t GetNbinsZ() { return 0; }
-    virtual inline double GetZLo() { return 0.0; }
-    virtual inline double GetZHi() { return 0.0; }
+    virtual inline size_t GetNbinsZ() { return 1; }
+    virtual inline double GetZLo() { return -0.5; }
+    virtual inline double GetZHi() { return GetNbinsZ() - 0.5; }
 };
 
 #endif
--- Processors/ORHistWriter.cc
+++ Processors/ORHistWriter.cc
@@ -58,6 +58,8 @@ ORDataProcessor::EReturnCode ORHistWriter::ProcessMyDataRecord(UInt_t* record)
 	  fHistDecoder->GetNbinsX(), fHistDecoder->GetXLo(), fHistDecoder->GetXHi(),
 	  fHistDecoder->GetNbinsY(), fHistDecoder->GetYLo(), fHistDecoder->GetYHi()
 	);
+	fHists[iHist]->SetXTitle(fHistDecoder->GetXTitle().c_str());
+	fHists[iHist]->SetYTitle(fHistDecoder->GetYTitle().c_str());
         break;
       case 3:
         fHists[iHist] = new TH3D(
@@ -66,6 +68,9 @@ ORDataProcessor::EReturnCode ORHistWriter::ProcessMyDataRecord(UInt_t* record)
 	  fHistDecoder->GetNbinsY(), fHistDecoder->GetYLo(), fHistDecoder->GetYHi(),
 	  fHistDecoder->GetNbinsZ(), fHistDecoder->GetZLo(), fHistDecoder->GetZHi()
 	);
+	fHists[iHist]->SetXTitle(fHistDecoder->GetXTitle().c_str());
+	fHists[iHist]->SetYTitle(fHistDecoder->GetYTitle().c_str());
+	fHists[iHist]->SetZTitle(fHistDecoder->GetZTitle().c_str());
         break;
       default:
         ORLog(kWarning) << "ProcessMyDataRecord(): Can't handle more than "
@@ -79,12 +84,27 @@ ORDataProcessor::EReturnCode ORHistWriter::ProcessMyDataRecord(UInt_t* record)
   }
 
   for(size_t iEntry=0; iEntry < fHistDecoder->GetNEntries(record); iEntry++) {
-    size_t iBin = hist->FindBin(
-      fHistDecoder->GetX(record, iEntry), 
-      fHistDecoder->GetY(record, iEntry), 
-      fHistDecoder->GetZ(record, iEntry)
-    );
-    hist->AddBinContent(iBin, fHistDecoder->GetWeight(record, iEntry));
+    switch(fHistDecoder->GetNDim()) {
+      case 1: 
+        hist->Fill(fHistDecoder->GetX(record, iEntry), 
+                   fHistDecoder->GetWeight(record, iEntry));
+        break;
+      case 2: 
+        ((TH2D*) hist)->Fill(fHistDecoder->GetX(record, iEntry), 
+                             fHistDecoder->GetY(record, iEntry), 
+                             fHistDecoder->GetWeight(record, iEntry));
+        break;
+      case 3: 
+        ((TH3D*) hist)->Fill(fHistDecoder->GetX(record, iEntry), 
+                             fHistDecoder->GetY(record, iEntry), 
+                             fHistDecoder->GetZ(record, iEntry),
+                             fHistDecoder->GetWeight(record, iEntry));
+        break;
+      default:
+        hist->Fill(fHistDecoder->GetX(record, iEntry), 
+                   fHistDecoder->GetWeight(record, iEntry));
+        break;
+    }
   }
 
   return kSuccess;
