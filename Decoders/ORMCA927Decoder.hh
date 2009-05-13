03d613c55725be2f2d70652d0e2fdfcf324862bc
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2009-05-13 14:26:44 +0000

    added the zdtMode
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@2054 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Decoders/ORMCA927Decoder.hh
+++ Decoders/ORMCA927Decoder.hh
@@ -4,7 +4,27 @@
 #define _ORMCA927Decoder_hh_
 
 #include "ORVDigitizerDecoder.hh"
-
+/*----------------------------------------------
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+ ^^^^ ^^^^ ^^^^ ^^-----------------------data id
+                  ^^ ^^^^ ^^^^ ^^^^ ^^^^-length in longs
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+                       ^-----------------zdt (1=ZDT, 0=Normal)
+                        ^----------------channel
+						  ^^^^ ^^^^ ^^^^-device
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx liveTime (20ms/bit, i.e. multiply by 0.02 to get seconds)
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx realTime (20ms/bit, i.e. multiply by 0.02 to get seconds)
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx 
+                                 ^-------zdt Spectra enabled
+                                  ^------zdt counting enabled
+                                    ^^^^-zdt speed
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx spare
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx spare
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx spare
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx spare
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx spare
+ the waveform follows and fills out the record 
+ ------------------------------------------------*/
 class ORMCA927Decoder: public ORVDigitizerDecoder
 {
   public:
@@ -20,8 +40,9 @@ class ORMCA927Decoder: public ORVDigitizerDecoder
 	inline UShort_t GetType()				{ return ( fDataRecord[1] & 0x00002000 ) >> 13;}
 	inline UShort_t GetChannelNum()			{ return ( fDataRecord[1] & 0x00001000 ) >> 12;}
 	inline UShort_t GetDevice()				{ return ( fDataRecord[1] & 0x00000fff );}
-	inline Double_t   GetLiveTime()			{ return (fDataRecord[2] * 0.020);}
-	inline Double_t   GetRealTime()			{ return (fDataRecord[3] * 0.020);}
+	inline Double_t GetLiveTime()			{ return (fDataRecord[2] * 0.020);}
+	inline Double_t GetRealTime()			{ return (fDataRecord[3] * 0.020);}
+	inline Int_t    GetZDTMode()			{ return (fDataRecord[4]);}
 	inline size_t   GetWaveformLen()		{ return (LengthOf(fDataRecord) - kBufHeadLen);}
 	inline UInt_t*  GetWaveformDataPointer(){ return (UInt_t*)(fDataRecord + kBufHeadLen);}
 	
