c47bb56e268809859c093b73755356b4af0a578a
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2009-05-14 19:18:01 +0000

    new spectrum decoder base class
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@2068 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Decoders/ORVMCADecoder.hh
@@ -0,0 +1,23 @@
+#ifndef _ORVMCADecoder_hh_
+#define _ORVMCADecoder_hh_
+#include <string>
+#include "TROOT.h"
+#include "ORVDataDecoder.hh"
+
+//! Defines an interface for MultiChannel Analysers decoders.
+class ORVMCADecoder: public ORVDataDecoder
+{
+  public:
+    ORVMCADecoder() {}
+    virtual ~ORVMCADecoder() {}
+
+    //! This length should be in number of words.
+    virtual size_t GetSpectrumLength() = 0;
+ 	virtual UInt_t	GetEventSpectrumPoint( size_t /*event*/, size_t spectrumPoint ) = 0;
+
+  protected:
+    UInt_t* fDataRecord;
+    
+};
+
+#endif
