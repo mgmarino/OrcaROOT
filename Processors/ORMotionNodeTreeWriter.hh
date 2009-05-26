30817be24ebd0f34f787664dce95e3d698701c67
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2009-05-26 19:04:28 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@2110 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Processors/ORMotionNodeTreeWriter.hh
@@ -0,0 +1,32 @@
+// ORMotionNodeTreeWriter.hh
+
+#ifndef __ORMotionNodeTreeWriter_hh__
+#define __ORMotionNodeTreeWriter_hh__
+
+#include "ORVTreeWriter.hh"
+#include "ORMotionNodeDecoder.hh"
+
+class ORMotionNodeTreeWriter : public ORVTreeWriter
+{
+  public:
+    ORMotionNodeTreeWriter(std::string treeName = "");
+    virtual ~ORMotionNodeTreeWriter();
+    enum ORMotionNodeTreeWriterConsts{kMaxwLength = 1000};
+    virtual EReturnCode ProcessMyDataRecord(UInt_t* record);
+    virtual inline void Clear() 
+		{	
+			fDevice			= 0;  
+			fChannel		= 0; 
+			fWaveformLength = 0;
+		}
+  protected:
+    virtual EReturnCode InitializeBranches();
+
+  protected:
+    ORMotionNodeDecoder* fEventDecoder;
+    UShort_t fDevice, fChannel;
+    UInt_t fWaveform[kMaxwLength];
+    size_t fWaveformLength;
+};
+
+#endif
