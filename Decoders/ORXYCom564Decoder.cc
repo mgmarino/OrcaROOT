dbe0b9f9540f503bcdabffacb83efd581557e0d2
mgmarino <mgmarino@fd86f90f-2c0a-422a-b972-84106d8db31b>
2011-12-02 14:50:20 +0000

    Adding decoder for the ORXYCom564 object.
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@6080 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Decoders/ORXYCom564Decoder.cc
@@ -0,0 +1,24 @@
+#include "ORXYCom564Decoder.hh"
+#include "ORLogger.hh"
+
+double ORXYCom564Decoder::ReferenceDateOf(UInt_t* record)
+{ 
+  if(LengthOf(record) < kXYCom564HeaderSize) return 0.0;
+
+  double refDate = record[2] + 1e-6*record[3];
+  return refDate;
+}
+
+UInt_t ORXYCom564Decoder::GetPar(UInt_t* record, size_t iPar, size_t iRow)
+{
+  switch(iPar) {
+    case 0: return CrateOf(record);
+    case 1: return CardOf(record);
+    case 2: return (record[iRow + kXYCom564HeaderSize] >> 16) & 0xFF;
+    case 3: return (record[iRow + kXYCom564HeaderSize] + 0xFF) & 0xFFFF ;
+    default:
+      ORLog(kWarning) << "GetPar(): index (" << iPar
+                      << ") out of range." << std::endl;
+      return 0;
+  }
+}
---
+++ Decoders/ORXYCom564Decoder.hh
@@ -0,0 +1,35 @@
+// ORXYCom564Decoder.hh
+
+#ifndef _ORXYCom564Decoder_hh_
+#define _ORXYCom564Decoder_hh_
+
+#include "ORVBasicADCDecoder.hh"
+
+class ORXYCom564Decoder : public ORVBasicADCDecoder
+{
+  protected: 
+    enum { kXYCom564HeaderSize = 4, 
+           kXYCom564MaxChannels = 64 } EXYCom564Consts;
+  public:
+
+    // Disable because we have more than one ADC value in a record 
+    virtual inline UInt_t ADCValueOf(UInt_t* /*record*/) { return 0; }
+
+    virtual double ReferenceDateOf(UInt_t* record);
+    virtual UInt_t GetPar(UInt_t* record, size_t iPar, size_t iRow);
+
+    virtual inline std::string GetLabel() { return "XYCom564"; }
+    virtual inline std::string GetDataObjectPath() { return "ORXYCom564Model:XYCom564"; }
+    virtual inline std::string GetDictionaryObjectPath() { return "ORXYCom564Model"; }
+    
+
+    virtual inline double GetXHi() { return double(0xffff) + 0.5; }
+    virtual inline size_t GetNChannelsMax() { return kXYCom564MaxChannels; }
+    virtual size_t GetNRows(UInt_t* record) 
+      { return (LengthOf(record) < kXYCom564HeaderSize) ? 
+          0 : (LengthOf(record) - kXYCom564HeaderSize); }
+
+
+};
+
+#endif /* _ORXYCom564Decoder_hh_ */
