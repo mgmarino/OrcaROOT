f0466050d6cb189eb935dfe95dbed2e1bb5f3163
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2012-06-12 13:50:01 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@7005 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Processors/ORDataGenBurstTreeWriter.cc
@@ -0,0 +1,58 @@
+// ORDataGenBurstTreeWriter.cc
+
+#include "ORDataGenBurstTreeWriter.hh"
+#include "ORLogger.hh"
+
+using namespace std;
+
+ORDataGenBurstTreeWriter::ORDataGenBurstTreeWriter(string treeName) : 
+ORVTreeWriter(new ORDataGenBurstDataDecoder, treeName)
+{
+	fBurstDecoder = dynamic_cast<ORDataGenBurstDataDecoder*>(fDataDecoder);
+	fCrate = 0;
+	fCard = 0;
+	fChannel = 0;
+	fValue = 0;
+	fTime = 0;
+	SetDoNotAutoFillTree();
+}
+
+ORDataGenBurstTreeWriter::~ORDataGenBurstTreeWriter()
+{
+	delete fBurstDecoder;
+}
+
+ORDataGenBurstTreeWriter::EReturnCode ORDataGenBurstTreeWriter::InitializeBranches()
+{
+	fTree->Branch("crate",   &fCrate,   "crate/I");
+	fTree->Branch("card",    &fCard,    "card/I");
+	fTree->Branch("channel", &fChannel, "channel/I");
+	fTree->Branch("value",   &fValue,   "value/I");
+	fTree->Branch("time",    &fTime,    "time/I");
+	return kSuccess;
+}
+
+ORDataGenBurstTreeWriter::EReturnCode ORDataGenBurstTreeWriter::ProcessMyDataRecord(UInt_t* record)
+{
+	// check severity to improve speed
+	if (ORLogger::GetSeverity() >= ORLogger::kDebug) { 
+	ORLog(kDebug)	<< "ProcessMyDataRecord(): got one: "
+					<< "crate-card-channel-value-time = " 
+					<< fBurstDecoder->CrateOf(record) << "-"
+					<< fBurstDecoder->CardOf(record) << "-" 
+					<< fBurstDecoder->ChannelOf(record) << "-" 
+					<< fBurstDecoder->ValueOf(record) << "-" 
+					<< fBurstDecoder->TimeOf(record) << endl;
+	}
+
+	fCrate   = fBurstDecoder->CrateOf(record);
+	fCard    = fBurstDecoder->CardOf(record);
+	fChannel = fBurstDecoder->ChannelOf(record);
+	fValue   = fBurstDecoder->ValueOf(record);
+	fTime    = fBurstDecoder->TimeOf(record);
+	fTree->Fill();
+  
+
+  return kSuccess;
+}
+
