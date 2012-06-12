ffaed84de81f6b02653b9cf5f08728dc944c9775
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2012-06-12 17:46:48 +0000

    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@7008 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Processors/ORDataGenBurstTreeWriter.cc
+++ Processors/ORDataGenBurstTreeWriter.cc
@@ -14,6 +14,8 @@ ORVTreeWriter(new ORDataGenBurstDataDecoder, treeName)
 	fChannel = 0;
 	fValue = 0;
 	fTime = 0;
+	fLastTime = 0;
+	fDiffTime = 0;
 	SetDoNotAutoFillTree();
 }
 
@@ -29,27 +31,41 @@ ORDataGenBurstTreeWriter::EReturnCode ORDataGenBurstTreeWriter::InitializeBranch
 	fTree->Branch("channel", &fChannel, "channel/I");
 	fTree->Branch("value",   &fValue,   "value/I");
 	fTree->Branch("time",    &fTime,    "time/I");
+	fTree->Branch("diffTime",&fDiffTime, "diffTime/I");
 	return kSuccess;
 }
 
 ORDataGenBurstTreeWriter::EReturnCode ORDataGenBurstTreeWriter::ProcessMyDataRecord(UInt_t* record)
 {
 	// check severity to improve speed
-	if (ORLogger::GetSeverity() >= ORLogger::kDebug) { 
-	ORLog(kDebug)	<< "ProcessMyDataRecord(): got one: "
-					<< "crate-card-channel-value-time = " 
-					<< fBurstDecoder->CrateOf(record) << "-"
-					<< fBurstDecoder->CardOf(record) << "-" 
-					<< fBurstDecoder->ChannelOf(record) << "-" 
-					<< fBurstDecoder->ValueOf(record) << "-" 
-					<< fBurstDecoder->TimeOf(record) << endl;
-	}
+
 
 	fCrate   = fBurstDecoder->CrateOf(record);
 	fCard    = fBurstDecoder->CardOf(record);
 	fChannel = fBurstDecoder->ChannelOf(record);
 	fValue   = fBurstDecoder->ValueOf(record);
 	fTime    = fBurstDecoder->TimeOf(record);
+
+	
+	if (ORLogger::GetSeverity() >= ORLogger::kDebug) { 
+		ORLog(kDebug)	<< "ProcessMyDataRecord(): got one: "
+		<< "crate-card-channel-value-time = " 
+		<< fBurstDecoder->CrateOf(record) << "-"
+		<< fBurstDecoder->CardOf(record) << "-" 
+		<< fBurstDecoder->ChannelOf(record) << "-" 
+		<< fBurstDecoder->ValueOf(record) << "-" 
+		<< fBurstDecoder->TimeOf(record) << "-"
+		<< fDiffTime << endl;
+	}
+	if(fLastTime == 0){
+		fLastTime = fTime;
+	}
+	else {
+		fDiffTime = fTime - fLastTime;
+		fLastTime = fTime;
+	}
+	
+	
 	fTree->Fill();
   
 
