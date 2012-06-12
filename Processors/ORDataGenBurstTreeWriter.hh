57665e439e2faa71bacc31ebf806fd003bfe72e9
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2012-06-12 13:50:06 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@7006 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Processors/ORDataGenBurstTreeWriter.hh
@@ -0,0 +1,28 @@
+// ORDataGenBurstTreeWriter.hh
+
+#ifndef _ORDataGenBurstTreeWriter_hh_
+#define _ORDataGenBurstTreeWriter_hh_
+
+#include "ORVTreeWriter.hh"
+#include "ORDataGenBurstDataDecoder.hh"
+
+class ORDataGenBurstTreeWriter : public ORVTreeWriter
+{
+  public:
+    ORDataGenBurstTreeWriter(std::string treeName = "");
+    virtual ~ORDataGenBurstTreeWriter();
+
+    virtual EReturnCode ProcessMyDataRecord(UInt_t* record);
+    virtual inline void Clear() 
+      { fCrate = 0; fCard = 0; fChannel = 0; fValue = 0;  fTime = 0; }
+
+  protected:
+    virtual EReturnCode InitializeBranches();
+
+  protected:
+    ORDataGenBurstDataDecoder* fBurstDecoder;
+    UShort_t fCrate, fCard, fChannel, fValue;
+	UInt_t fTime;
+};
+
+#endif
