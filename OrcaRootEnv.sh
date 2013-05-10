a31c58abd9fd2004085f82c195f94f7b852a7d56
kjw <kjw@fd86f90f-2c0a-422a-b972-84106d8db31b>
2013-05-10 20:30:28 +0000

    Fixed the build. Forgot to add some CMake Files and pthreads now work on non-Darwin enavbled build systems
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@8267 fd86f90f-2c0a-422a-b972-84106d8db31b

--- OrcaRootEnv.sh
+++ OrcaRootEnv.sh
@@ -2,7 +2,7 @@
  
 #Sets up the OrcaRoot Environment Variables
 
-export ORDIR=@CMAKE_INSTALL_PREFIX@
+export ORDIR=/home/kjw/Dev/kjw_software/install
 export PATH=$ORDIR/bin\:$PATH
 export DYLD_LIBRARY_PATH=$ORDIR/lib\:$DYLD_LIBRARY_PATH
 #TODO: PKG-Config
