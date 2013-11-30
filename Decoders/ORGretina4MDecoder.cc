f5b12d700e42fcc62368746cb411e9e8e9e59919
jasondet <jasondet@fd86f90f-2c0a-422a-b972-84106d8db31b>
2013-11-30 02:48:46 +0000

    Adding draft decoder for Gretina4M firmware. Advanced features like multi-sampling with bit shifting not yet implemented. Jason
    git-svn-id: svn://orca.physics.unc.edu/OrcaRoot@8761 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Decoders/ORGretina4MDecoder.cc
@@ -0,0 +1,91 @@
+// ORGretaDecoder.cc
+
+#include "ORGretina4MDecoder.hh"
+#include "ORLogger.hh"
+#include "ORUtils.hh"
+
+using namespace std;
+
+ORGretina4MDecoder::ORGretina4MDecoder() 
+{
+}
+
+bool ORGretina4MDecoder::SetDataRecord(UInt_t* dataRecord) 
+{
+  if(IsShort(dataRecord)) {
+    ORLog(kDebug) << "SetDataRecord(): data record is short form, invalid" << endl;
+    fDataRecord = NULL;
+    return false;
+  }
+  fDataRecord = dataRecord;
+  ORLog(kError) << GetChannelParameter(kChPreSum, CrateOf(), CardOf(), GetEventChannel(0));
+  return true;
+}
+
+void ORGretina4MDecoder::SetDecoderDictionary(const ORDecoderDictionary* dict)
+{
+  ORVDataDecoder::SetDecoderDictionary(dict);
+  for(UInt_t iPar = 0; iPar < kNPars; iPar++) fCardPars[iPar].clear();
+}
+
+UInt_t ORGretina4MDecoder::GetCardParameter(ECardPars par, UInt_t crate, UInt_t card)
+{
+  UInt_t ccc = (crate << 12) + (card << 4); // 0xfffff000 + 0xff0
+  return GetParameter(UInt_t(par), ccc);
+}
+
+UInt_t ORGretina4MDecoder::GetChannelParameter(EChanPars par, UInt_t crate, UInt_t card, UInt_t channel)
+{
+  UInt_t ccc = (crate << 12) + (card << 4) + channel; // 0xfffff000 + 0xff0 + 0xf
+  return GetParameter(UInt_t(par), ccc);
+}
+
+UInt_t ORGretina4MDecoder::GetParameter(UInt_t par, UInt_t ccc)
+{
+  map<UInt_t, UInt_t>::iterator iter = fCardPars[par].find(ccc);
+
+  //return if found
+  if(iter != fCardPars[par].end()) return iter->second;
+
+  // otherwise we have to look up the info from the header
+  // first find a key
+  const char* key = "";
+  if(par == kIntTime) key = "Integration Time";
+  //if(par == kChPreSum) key = "Chpsrt";
+  if(par == kChPreSum) key = "Enabled";
+  if(par == kChPreSum) key = "Mrpsrt";
+  else {
+    ORLog(kError) << "Unknown card parameter " << par << endl;
+    return (UInt_t) -1;
+  }
+
+  // now pull out the value for the key
+  UInt_t cr = ccc >> 12;
+  UInt_t ca = (ccc & 0xff0) >> 4;
+  UInt_t ch = (ccc & 0xf);
+  UInt_t value = 0;
+  if(par < kNCardPars) value = GetIntValueFromKey(key, cr, ca);
+  else value = dynamic_cast<const ORDictValueI*>(GetArrayFromKey(key, cr, ca)->At(ch))->GetI();
+
+  // cache locally and return
+  fCardPars[par][ccc] = value;
+  return value;
+}
+
+ULong64_t ORGretina4MDecoder::GetTimeStamp(UInt_t* header)
+{
+  ULong64_t time = header[3] & 0xffff;
+  time = time << 32;
+  time += header[2];
+  return time;
+}
+
+UInt_t ORGretina4MDecoder::GetEnergy(UInt_t* header)
+{ 
+  UInt_t energy = header[3] >> 16;
+  energy += (header[4] & 0x000001ff) << 16;
+  //energy is in 2's complement, take abs value if necessary
+  if (energy & 0x1000000) energy = (~energy & 0x1ffffff) + 1;
+  return energy;
+}
+
---
+++ Decoders/ORGretina4MDecoder.hh
@@ -0,0 +1,90 @@
+// ORGretaDecoder.hh
+
+#ifndef _ORGretina4MDecoder_hh_
+#define _ORGretina4MDecoder_hh_
+
+#include <map>
+#include "ORVDigitizerDecoder.hh"
+
+/*
+  Decoder for MJD-specific firmware for the Gretina digitizer. This firmware
+  delivers a hybrid waveform with different sampling frequencies before and
+  after a window surrounding the trigger on the rising edge. In addition to
+  providing the 
+*/ 
+   
+
+class ORGretina4MDecoder : public ORVDigitizerDecoder
+{
+  public:
+    enum EGretina4MConsts { kEventDataLen = 1024, kEventHeaderLen = 15, kWFLen = 2018 };
+    enum ECardPars { 
+      kIntTime = 0, // Integration time (rise time) of trap filter
+      kNCardPars
+    };
+    enum EChanPars { 
+      kChPreSum = kNCardPars, // channel presum (n samples)
+      kMRPreSum, // additional presum for multi-rate mode (i.e. for baseline, flattop)
+      kNPars, 
+      kNChanPars = kNPars-kNCardPars
+    };
+
+    ORGretina4MDecoder();
+    virtual ~ORGretina4MDecoder() {}
+
+    virtual inline std::string GetDataObjectPath() { return "ORGretina4M:Gretina4M"; }  
+    virtual inline std::string GetDictionaryObjectPath() { return "ORGretina4M"; }  
+
+    // Complete the set of functions satisfying the ORVDigitizerDecoder
+    // interface. Gives access to the raw (hybrid) waveform; the
+    // GetSamplingFrequency() response is for the underlying ADC speed. Use the
+    // channel-specific interface below for unambiguous access to the "energy"
+    // and "rising edge" waveforms, correcting for presumming, etc.
+    virtual bool SetDataRecord(UInt_t* dataRecord);
+    virtual inline double GetSamplingFrequency() { return 0.1; } // in GHz. 
+    virtual inline UShort_t GetBitResolution() { return 14; }
+    virtual inline size_t GetNumberOfEvents() { return (LengthOf(fDataRecord)-2)/kEventDataLen; }
+
+    virtual inline ULong64_t GetEventTime(size_t iEvent) { return GetTimeStamp(EP(iEvent)); }
+    virtual inline UInt_t GetEventEnergy(size_t iEvent) { return GetEnergy(EP(iEvent)); }
+    virtual inline UShort_t GetEventChannel(size_t iEvent) { return GetChannel(EP(iEvent)); }
+    virtual inline size_t GetEventWaveformLength(size_t) { return kWFLen; }
+    virtual inline UInt_t GetEventWaveformPoint(size_t iEvent, size_t iSample) { return WFPS(iEvent)[iSample]; }
+
+    // energy waveform: pre-sum and shift every N samples of rising-edge portion
+    // as necessary to get one waveform of constant sampling frequency
+    //virtual double GetEnergyWFSamplingFrequency(); // in GHz. 
+
+    // rising edge waveform: access to everything that is at the highest
+    // sampling frequency
+    //virtual double GetRisingEdgeWFSamplingFrequency(); // in GHz. 
+
+    // Functions related to setting / accessing card parameters)
+    virtual void SetDecoderDictionary(const ORDecoderDictionary* dict);
+    virtual UInt_t GetCardParameter(ECardPars par, UInt_t crate, UInt_t card); 
+    virtual UInt_t GetChannelParameter(EChanPars par, UInt_t crate, UInt_t card, UInt_t channel); 
+    virtual inline UInt_t GetEnergyNormalization()
+      { return GetCardParameter(kIntTime, CrateOf(), CardOf()); }
+
+  protected:
+    // fast pointer to event data
+    virtual UInt_t* EP(size_t iEvent) { return fDataRecord + 2 + iEvent*kEventDataLen; } 
+
+    // fast pointers to waveform data
+    virtual UInt_t* WFP(size_t iEvent) { return EP(iEvent)+kEventHeaderLen; }
+    virtual UShort_t* WFPS(size_t iEvent) { return (UShort_t*) WFP(iEvent); }
+
+    // event header decoding
+    virtual ULong64_t GetTimeStamp(UInt_t* header); // time of trigger, in clock ticks
+    virtual UInt_t GetEnergy(UInt_t* header);
+    virtual UShort_t GetChannel(UInt_t* header) { return header[1] & 0xf; }
+
+    // internal interface for parameters
+    virtual UInt_t GetParameter(UInt_t par, UInt_t ccc);
+
+  protected:
+    std::map< UInt_t, std::map<UInt_t, UInt_t> > fCardPars;
+};
+
+#endif
+
