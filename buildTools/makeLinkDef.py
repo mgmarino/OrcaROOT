44e2488dc6477af8e2f588fff7e661c8f29ba6e3
Ian Guinn <iguinn@uw.edu>
2015-02-13 15:57:50 -0800

    Updated makeLinkDef.py for python3 compatibility.

--- buildTools/makeLinkDef.py
+++ buildTools/makeLinkDef.py
@@ -44,9 +44,9 @@ def do_all_files(output_name, build_base, all_files, input_linkdef = ""):
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "o:b:i", ["output=", "basebuild=", "input_linkdef="])
-    except getopt.GetoptError, err:
+    except getopt.GetoptError as err:
         # print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
+        print(str(err)) # will print something like "option -a not recognized"
         sys.exit(2)
     output_name = ""
     build_base  = ""
